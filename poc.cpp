457c05ccb83a70f2f97f53c17aea6448acc884d1
Lukasz Towarek <lukasz.towarek@gmail.com>
2025-03-19 00:34:20 +0000

    Integrate antlr4

--- poc.cpp
+++ poc.cpp
@@ -1,7 +1,26 @@
-#include <fmt/core.h>
+#include <iostream>
 
-int main()
-{
-    fmt::print("Hello World!\n");
-    return 0;
+#include "antlr4-runtime.h"
+#include "TLexer.h"
+#include "TParser.h"
+
+using namespace poc;
+using namespace antlr4;
+
+int main(int , const char **) {
+  ANTLRInputStream input(u8"🍴 = 🍐 + \"😎\";(((x * π))) * µ + ∰; a + (x * (y ? 0 : 1) + z);");
+  TLexer lexer(&input);
+  CommonTokenStream tokens(&lexer);
+
+  tokens.fill();
+  for (auto token : tokens.getTokens()) {
+    std::cout << token->toString() << std::endl;
+  }
+
+  TParser parser(&tokens);
+  tree::ParseTree* tree = parser.main();
+
+  std::cout << tree->toStringTree(&parser) << std::endl << std::endl;
+
+  return 0;
 }
